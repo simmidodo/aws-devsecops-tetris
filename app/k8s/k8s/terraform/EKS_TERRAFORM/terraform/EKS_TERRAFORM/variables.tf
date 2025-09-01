7097a4d4a30bf5dbebb28a211190df6ba0febab9
Simran <133814475+simmidodo@users.noreply.github.com>
2025-09-01 01:59:45 +0100

    Create variables.tf

---
+++ app/k8s/k8s/terraform/EKS_TERRAFORM/terraform/EKS_TERRAFORM/variables.tf
@@ -0,0 +1,11 @@
+variable "project" {
+  description = "Project tag/name"
+  type        = string
+  default     = "tetris"
+}
+
+variable "region" {
+  description = "AWS region (no deploy yet)"
+  type        = string
+  default     = "ap-south-1"
+}
